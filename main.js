9fc253348fec79ea5937167530df8b13a3f6db0e
Sara Fira <sarafira26@gmail.com>
2026-05-15 22:50:08 +0300

    Update and rename My_First_Project_(JavaScript) to main.js

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 const input = require('sync-input');
 
-// We have imported the 'sync-input' package for you.
-// This package allows you to get user input.
-// Like so:
-// let name = input("Type your name: ");
-// let age = Number(input("Type your age: "));
-// You will need it in later stages.
-
 const earnings = {
   "Bubblegum": 202,
   "Toffee": 118,
